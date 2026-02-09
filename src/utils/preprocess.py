c73930e9006f15eeb7e2b992592ce1fd00c83d4f
Simhabits <2024aa05131@wilp.bits-pilani.ac.in>
2026-02-09 11:17:46 +0530

    Preprocess image

--- src/utils/preprocess.py
+++ src/utils/preprocess.py
@@ -2,9 +2,15 @@ from PIL import Image
 import torch
 from torchvision import transforms
 
+# IMPORTANT:
+# This MUST match the preprocessing used during training
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
 ])
 
 def preprocess_image(image: Image.Image) -> torch.Tensor:
